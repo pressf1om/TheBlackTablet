a8ed7a673ee32df888d48d1d95be48b3363cb7de
nikitanikonov <loser.alanwolker2005@gmail.com>
2024-09-15 15:29:58 +0300

    Исправлен фон бокового меню. Создалась облость для карточки.

--- Front-end/MainPage.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('menu-icon').addEventListener('click', function()
-{
-    var menu = document.getElementById('menu');
-    if (menu.style.display === 'flex') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'flex';
-    }
-});
