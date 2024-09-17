3799233288970e341484953f905166ea689b9241
Николай Белицкий <belkaleonidov@gmail.com>
2024-09-17 15:49:12 +0300

    изменена структура проекта, удалена головная папка static, вся статика и темплейты теперь в приложениях, добавлен файл endpoint.txt

--- TheBlackTablet/TheBlackTablet/settings.py
+++ TheBlackTablet/TheBlackTablet/settings.py
@@ -47,7 +47,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [ BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -109,9 +109,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
